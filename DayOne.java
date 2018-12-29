adc0dc13d917bd0f0180e6a7d5a3408a71a09b8a
aravindanath <aravindanath86@gmail.com>
2018-12-29 10:10:30 +0530

    Adding new DayOne java file

---
+++ DayOne.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+
+		int a = 1231231231;
+		System.out.println(a);
+		double d  = 13412341234213.00;
+		float f = 3424.09f;
+	 
+		
+		char c = 's';
+		
+		long l = 2345234513451351345l;
+	System.out.println(	Math.random());;
+		
+		String s  = "Pradyumna";
+		String b = "Bangalore";
+		System.out.println(s + " " +b);
+	
+
+	}
+	 
+	public int addNumber(int a, int b) {
+		DayOne pra = new DayOne();
+		int sum = a + b;
+		return sum;
+	}
+
+}
